ffa24500c3e8d4eae4d2d9953f2398725b6bb86b
Ganesh <gpujari@rikoouu.com>
2018-01-05 23:30:04 +0530

    wip

--- base/views.py
+++ base/views.py
@@ -4,26 +4,45 @@ from base.models import Shirts,Jeans,Jackets,Gymwear,Sunglasses,Watches
 
 def products(request):
     context = {}
-    try:
-        shirts=Shirts.objects.filter(is_active=True)
-        context["shirts"]=shirts
-
-        jeans = Jeans.objects.filter(is_active=True)
-        context["jeans"]=jeans
-
-        jackets = Jackets.objects.filter(is_active=True)
-        context["jackets"]=jackets
-
-        gymwear = Gymwear.objects.filter(is_active=True)
-        context["gymwear"]=gymwear
-
-        sunglasses = Sunglasses.objects.filter(is_active=True)
-        context["sunglasses"]=sunglasses
-
-        watches = Watches.objects.filter(is_active=True)
-        context["watches"]=watches
+    return render(request, 'index.html', context)
 
 
-        return render(request, 'index.html', context)
-    except (Exception,)as e :
-        print (e)
+def home(request):
+    context={}
+    return render(request, 'home.html', context)
+
+def shirts(request):
+    context={"title":"Shirts"}
+    shirts = Shirts.objects.filter(is_active=True)
+    context["objs"] = shirts
+    return render(request, 'shirts.html', context)
+
+def jeans(request):
+    context={"title":"Jeans"}
+    jeans = Jeans.objects.filter(is_active=True)
+    context["objs"] = jeans
+    return render(request, 'shirts.html', context)
+
+def jacket(request):
+    context={"title":"Jackets"}
+    jackets = Jackets.objects.filter(is_active=True)
+    context["objs"] = jackets
+    return render(request, 'shirts.html', context)
+
+def gymwear(request):
+    context={"title":"Jacket"}
+    objs = Gymwear.objects.filter(is_active=True)
+    context["objs"] = objs
+    return render(request, 'shirts.html', context)
+
+def watches(request):
+    context={"title":"Watches"}
+    objs = Watches.objects.filter(is_active=True)
+    context["objs"] = objs
+    return render(request, 'shirts.html', context)
+
+def sunglasses(request):
+    context={"title":"Sunglasses"}
+    objs = Sunglasses.objects.filter(is_active=True)
+    context["objs"] = objs
+    return render(request, 'shirts.html', context)
--- product1/urls.py
+++ product1/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from base.views import products
+from base.views import products,home,shirts,jeans,jacket,gymwear,watches
 from product1 import settings
 import django
 from django.views.static import serve
@@ -8,6 +8,13 @@ from django.views.static import serve
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/home/$', products),
+    # url(r'^api/home/$', products),
+    url(r'^api/home/$', home),
+    url(r'^api/shirts/$', shirts),
+    url(r'^api/jeans/$', jeans),
+    url(r'^api/jackets/$', jacket),
+    url(r'^api/gymwear/$', gymwear),
+    url(r'^api/watches/$', watches),
+    url(r'^api/sunglasses/$', watches),
     url(r'^pics/(?P<path>.*)$',django.views.static.serve,{'document_root': settings.BASE_DIR+'/pics'}),
 ]
